36e516d6aede49bea1d5af98718a1403ea465177
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-20 11:02:24 +0300

    wrap around when reaching end of alphabet

--- script.rb
+++ script.rb
@@ -2,7 +2,13 @@ def caesar_cipher(string, shift_num)
   temp = string.split("")
   final = temp.map do |letter|
     if letter.match?(/[[:alpha:]]/)
-      (letter.ord - shift_num).chr
+      if (letter.ord + shift_num).between?(97, 122) || (letter.ord + shift_num).between?(65, 90)
+      (letter.ord + shift_num).chr
+      elsif (letter.ord - shift_num) < 97
+        (97 + ((letter.ord + shift_num) - 122) - 1).chr
+      else
+        (65 + ((letter.ord + shift_num) - 90) - 1).chr
+      end
     else
       letter
     end  
@@ -10,6 +16,4 @@ def caesar_cipher(string, shift_num)
   final.join("")
 end
 
-puts caesar_cipher("ABC! abc XYZ xyz?", 2)
-
-# a on 97, z on 122 | A on 65, Z on 90
+puts caesar_cipher("Todellisuus voi olla outoa, uskotko?", 8)
